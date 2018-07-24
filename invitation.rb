73029cc22e4bd5e79dcb6f0b2e494f493a6402aa
me9hana <meghanagoli@gmail.com>
2018-07-24 14:53:56 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,10 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
+puts "Dear #{guest_name},
+
+You are cordially invited to #{party_name} on #{date} at #{time}. Please RSVP no later than #{deadline}.
+
+Sincerely,
+
+#{host_name}"
